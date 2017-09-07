77c6b59de9564bf3430b406816197b39ed5a50ac
keerthanashanmugam <keerthismart555@gmail.com>
2017-09-07 16:58:11 +0530

    ERR: hinge loss function not getting updated

--- newnnet.py
+++ newnnet.py
@@ -1,3 +1,4 @@
+#FaceVerification/newnnet.py
 
 import tensorflow as tf
 import pandas as pd
@@ -6,7 +7,7 @@ from scipy.misc import imread
 from sklearn.metrics import accuracy_score
 
 tou = tf.constant(3.0, dtype=tf.float32)
-mu = 100
+mu = 0.001
 lamda = tf.constant(0.01, dtype=tf.float32)
 beta = tf.constant(0.01, dtype=tf.float32)
 splitsize = 5401
@@ -20,10 +21,10 @@ output_num_units = 100
 epochs = 10
 batch_size = 6000
 
-#keerthana
+
 
 #data = pd.read_csv('../Matlab/data.csv',sep = "\t")
-f = open ( '../Matlab/trail.csv' , 'r')
+f = open ( '../../Matlab/trail.csv' , 'r')
 data = np.array( [ map(float,line.split('\t')) for line in f ])
 
 
@@ -61,6 +62,32 @@ biases = {
     'output': tf.Variable(outPlace_B)
 }
 
+mean_h = - np.sqrt(6)/np.sqrt(dummy + hidden_num_units)
+stddev_h = np.sqrt(6)/np.sqrt(dummy + hidden_num_units)
+
+mean_o = - np.sqrt(6)/np.sqrt(hidden_num_units + output_num_units)
+stddev_o = np.sqrt(6)/np.sqrt(hidden_num_units + output_num_units)
+
+# weights = {
+#     'hidden': tf.Variable(tf.zeros( [dummy,hidden_num_units])),
+#     'output': tf.Variable(tf.zeros([hidden_num_units,output_num_units]))
+# }
+
+
+# biases = {
+#     'hidden': tf.Variable(tf.zeros([hidden_num_units])),
+#     'output': tf.Variable(tf.zeros([output_num_units]))
+# }
+
+# weights = {
+#     'hidden': tf.Variable(tf.random_normal( [dummy,hidden_num_units], mean =mean_h,stddev = stddev_h)),
+#     'output': tf.Variable(tf.random_normal([hidden_num_units,output_num_units], mean = mean_o,stddev = stddev_o ))
+# }
+
+# biases = {
+#     'hidden': tf.Variable(tf.random_normal([hidden_num_units], mean =mean_h,stddev = stddev_h)),
+#     'output': tf.Variable(tf.random_normal([output_num_units], mean = mean_o,stddev = stddev_o))
+# }
 
 def ge(param):
 	print param*beta
@@ -70,20 +97,21 @@ def ge(param):
 def cost_function(out,l):
 	
 
-	# temp = tf.subtract(tou , out)
+	temp = tf.subtract(tou , out)
 
-	s = (0.5) * tf.losses.hinge_loss(out ,l)
+    # s = tf.nn.relu(temp)
+	s = (0.5) * tf.losses.hinge_loss(temp ,l)
 	# s = (0.5) * tf.reduce_sum(tf.map_fn(ge , temp ) )
 	# for i in weights.keys():
 	# 	s += (lamda/2)* tf.reduce_sum(tf.pow(weights[i],2))
 	# for i in biases.keys():
 	# 	s += (lamda/2)* tf.reduce_sum(tf.pow(biases[i],2))
-	return s
+	# return s
 
 def reg():
     s = 0
-    for i in weights.keys():
-      s += (lamda/2)* tf.reduce_sum(tf.pow(weights[i],2))
+    # for i in weights.keys():
+    #   s += (lamda/2)* tf.reduce_sum(tf.pow(weights[i],2))
     for i in biases.keys():
       s += (lamda/2)* tf.reduce_sum(tf.pow(biases[i],2))  
     return s
@@ -93,38 +121,60 @@ print 'making layers now'
 ### define weights and biases of the neural network (refer this article if you don't understand the terminologies)
 
 
-hidden_layer1 = tf.add(tf.matmul(x[:,:7080], weights['hidden']), biases['hidden'])
-hidden_layer1 = tf.nn.tanh(hidden_layer1)                                                                                                                                                                           
+hidden_layer1_ = tf.add(tf.matmul(x[:,:7080], weights['hidden']), biases['hidden'])
+hidden_layer1 = tf.nn.tanh(hidden_layer1_)                                                                                                                                                                           
 
-output_layer1 = tf.add(tf.matmul(hidden_layer1, weights['output']) , biases['output'])
+output_layer1_ = tf.add(tf.matmul(hidden_layer1, weights['output']) , biases['output'])
+output_layer1 = tf.nn.tanh(output_layer1_)
 
-hidden_layer2 = tf.add(tf.matmul(x[:,7080:], weights['hidden']), biases['hidden'])
-hidden_layer2 = tf.nn.tanh(hidden_layer2)
+hidden_layer2_ = tf.add(tf.matmul(x[:,7080:], weights['hidden']), biases['hidden'])
+hidden_layer2 = tf.nn.tanh(hidden_layer2_)
 
-output_layer2 = tf.add(tf.matmul(hidden_layer2, weights['output']) , biases['output'])
-
+output_layer2_ = tf.add(tf.matmul(hidden_layer2, weights['output']) , biases['output'])
+output_layer2 = tf.nn.tanh(output_layer2_)
 output_layer = tf.reduce_sum(tf.pow(tf.subtract(output_layer1,output_layer2),2),1,keep_dims=True)
 
 #cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = output_layer,labels = y))
 
 print 'made layers now cost function'
 
-cost = cost_function(output_layer,y)
+cost = (0.5) * tf.losses.hinge_loss( tf.subtract(tou , output_layer),y)
+# cost = tf.reduce_mean(tf.nn.relu(1- tf.multiply(tf.subtract(tou , output_layer) ,y) ))
+# cost = cost_function(output_layer,y)
 
-optimizer = tf.train.GradientDescentOptimizer(learning_rate= 0.1).minimize(cost)
+#cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = output_layer, labels = y))
+
+# opt = tf.train.GradientDescentOptimizer(learning_rate= 0.01)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate= 10000000).minimize(cost,var_list = [weights,biases])
+
+# ################################ TESTING PURPOSE
+# grads = opt.compute_gradients(cost,var_list = [weights,biases])
+
+
+
+
+# ################################
+
 
 init = tf.initialize_all_variables()
 
 print 'seeesion is going to start....'
 
-embHid_W = np.random.normal(0.0,0.1, (dummy, hidden_num_units) )
-embHid_B = np.random.normal(0.0,0.1, (hidden_num_units))
-embOut_W = np.random.normal(0.0,0.1, (hidden_num_units, output_num_units))
-embOut_B = np.random.normal(0.0,0.1, (output_num_units))
+embHid_W = np.random.normal(mean_h,stddev_h, (dummy, hidden_num_units) )
+embHid_B = np.zeros(hidden_num_units,dtype = np.float32)
+embOut_W = np.random.normal(mean_o,stddev_o, (hidden_num_units, output_num_units))
+embOut_B = np.zeros(output_num_units , dtype = np.float32)
 
+# #########################################################################################
+# sess = tf.Session()
+    
+# sess.run(init,feed_dict = {hidPlace_W: embHid_W , hidPlace_B: embHid_B , outPlace_W: embOut_W , outPlace_B : embOut_B, x:train_x , y:train_y})
 
+# grad_vals = sess.run([grad[0] for grad in grads])
+# #########################################################################################
 
 with tf.Session() as sess:
+
     # create initialized variables
 
     # print sess.run(tf.shape(output_layer))
@@ -132,13 +182,23 @@ with tf.Session() as sess:
 
     sess.run(init,feed_dict = {hidPlace_W: embHid_W , hidPlace_B: embHid_B , outPlace_W: embOut_W , outPlace_B : embOut_B})
     
+    # sess.run(init,feed_dict = {hidPlace_W: embHid_W , hidPlace_B: embHid_B , outPlace_W: embOut_W , outPlace_B : embOut_B, x:train_x , y:train_y})
+
+   
     ### for each epoch, do:
     ###   for each batch, do:
     ###     create pre-processed batch
     ###     run optimizer by feeding batch
     ###     find cost and reiterate to minimize
-    
-    for epoch in range(epochs):
+
+
+    print "\n weights" , sess.run(weights['hidden'])
+    print "\n weights" , sess.run(biases['hidden'])
+
+    cost1 = 0
+    cost2 = -10
+    count = 0
+    while( ( cost1-cost2 > 0.01 or count<2 ) and count < 10):
         avg_cost = 0
         total_batch = int(data.shape[0]/batch_size)
         for i in range(total_batch):
@@ -146,21 +206,27 @@ with tf.Session() as sess:
             
             batch_x = train_x
             batch_y = train_y
-            # print sess.run(tf.shape(batch_x))
-            # print sess.run(tf.shape(batch_y))
-            # print sess.run(tf.shape(output_layer))
-            # print sess.run(tf.shape(batch_y))
+
             _, c = sess.run([optimizer, cost], feed_dict = {x: batch_x, y: batch_y})
             
             avg_cost += c / total_batch
         #regu = reg()
-        print "regularisation: ", sess.run(regu)
-        print "Epoch:", (epoch+1), "cost =", "{:.5f}".format(avg_cost) , total_batch
-    
+        print "regularisation: ", sess.run(reg())
+        print "Epoch:", (count+1), "cost =", "{:.5f}".format(avg_cost) , total_batch
+        cost1 = cost2
+        cost2 = avg_cost
+        count += 1
+
     print "\nTraining complete!"
-    pred = tf.equal(tf.less(output_layer,tou,name = None),val_y)
+
+    tem = tf.reduce_mean(output_layer)
+    pred = tf.equal(tf.less(output_layer,tem,name = None),val_y)
+
+    print "\n weights" , sess.run(weights['hidden'])
+    print "\n weights" , sess.run(biases['hidden'])
 
     accuracy = tf.reduce_mean(tf.cast(pred, "float"))
+    print "\noutputs: ", output_layer1_.eval({x: val_x,y:val_y})
     print "Validation Accuracy:", accuracy.eval({x: val_x, y: val_y})
 #    find predictions on val set
 #    pred_temp = tf.equal(tf.argmax(output_layer, 1), tf.argmax(y, 1))
